b86727246066bec1dd38e1cad2267464df03971d
Leon <salient@Full-Stack-SEO.local>
2021-06-22 22:13:19 -0400

    updated FilePicker example

--- examples/FilePicker/filepicker.nim
+++ examples/FilePicker/filepicker.nim
@@ -1,21 +1,18 @@
 import neel, os, random
 
+randomize()
+
 exposeProcs:
+    
     proc filePicker(directory: string) =
-
         let absPathDir = absolutePath(directory,root=getHomeDir())
-
         if dirExists(absPathDir):
-            randomize() 
-
             var files: seq[string]
             for kind, path in absPathDir.walkDir:
                 files.add(path)
-
             callJs("showText",sample(files))
-
         else:
             callJs("showText", "The directory you chose does not exist. Does it stem from the Home Directory?")
 
 
-startApp(startURL="index.html",assetsDir="web",size=[400,250])
+startApp(size=[400,250])
